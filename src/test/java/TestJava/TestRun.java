0315e66d39ea2fbfe5c5c167359bdc93aac7b58b
Globalapp <testsids2@gmail.com>
2016-03-22 19:32:15 +0530

    First commit

--- src/test/java/TestJava/TestRun.java
+++ src/test/java/TestJava/TestRun.java
@@ -4,9 +4,12 @@ import java.io.IOException;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -14,7 +17,7 @@ public class TestRun {
 private WebDriver driver;
 private WebDriverWait wait;
 
-@BeforeTest
+@BeforeMethod
 public void setUp()
 {
 	 driver = new FirefoxDriver();
@@ -22,11 +25,7 @@ public void setUp()
      driver.manage().window().maximize();
      driver.get("https://accounts.google.com/ServiceLogin?service=mail&passive=true&rm=false&continue=https://mail.google.com/mail/&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1#identifier");
 }
-@AfterTest
-public void teardown() throws IOException
-{
-	driver.quit();
-}
+
 
 @Test
 public void Login() throws IOException, InterruptedException{
@@ -36,10 +35,16 @@ public void Login() throws IOException, InterruptedException{
 	Thread.sleep(2000);
 	driver.findElement(By.id("Passwd")).clear();
 	driver.findElement(By.id("Passwd")).sendKeys("sachin2727");
-	Thread.sleep(20000);
-	driver.findElement(By.name("signIn")).click();
-	Thread.sleep(50000);
+	Thread.sleep(2000);
+	driver.findElement(By.id("signIn")).click();
+	Thread.sleep(5000);
 	
+}
+
+@AfterMethod
+public void teardown() throws IOException
+{
+	driver.quit();
 }
 
 }
